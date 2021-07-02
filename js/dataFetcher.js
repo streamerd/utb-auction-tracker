dbeb2632a68eb69325dc46139ab6ce6c39d9edf3
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-07-02 14:40:06 +0200

    add bid history with example edition  (#102775) from ko

--- js/dataFetcher.js
+++ js/dataFetcher.js
@@ -2,7 +2,7 @@ var events = [];
 
 function auctionEvents() {
     try {
-        const response = fetch('https://api.thegraph.com/subgraphs/name/knownorigin/known-origin', {
+        fetch('https://api.thegraph.com/subgraphs/name/knownorigin/known-origin', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -29,12 +29,111 @@ function auctionEvents() {
 }
 }
 
+function getBiddingHistory(editionId) {
+   try {
+        fetch('https://api.thegraph.com/subgraphs/name/knownorigin/known-origin', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              query: `
+              {
+                edition(id: ${editionId}){
+                  id
+                  metadataName
+                  metadataArtist
+                  metadataArtistAccount
+                  totalAvailable
+                  totalSold
+                  artist{
+                    id
+                  }
+                  biddingHistory{
+                    name
+                    timestamp
+                    transactionHash
+                    caller
+                    bidder
+                    ethValue
+                  }
+                  transfers{
+                    timestamp
+                    transactionHash
+                  }
+                  activeBid{
+                    timestamp
+                  }
+                }
+              }
+           `
+
+            }),
+          }).then((res) => res.json())
+            // .then((result) => console.log(result.data['auctionEvents'][0]));
+            .then((result) => {
+              writeArtworkDetails(result.data);
+              writeBiddindHistory(result.data);
+            });
+
+} catch (error) {
+       console.log(error)
+}
+}
+
+function writeArtworkDetails(edition) {
+  editionData = edition["edition"];
+  editionId = editionData["id"];
+  artworkName = editionData["metadataName"];
+  artistName = editionData["metadataArtist"];
+
+  console.log(`
+  edition id: ${editionId} \n
+  artwork name: ${artworkName} \n
+  artist name: ${artistName} \n
+  `);
+
+  document.getElementById("artwork-name").innerHTML = artworkName
+
+}
+function writeBiddindHistory(edition) {
+  editionData = edition['edition']
+  biddings = editionData["biddingHistory"]
+
+  firstBidding = biddings[0]
+
+  bidName = firstBidding["name"]
+  bidder = firstBidding["bidder"];
+  timestamp = epochToLocalDatetime(firstBidding["timestamp"]); // convert epoch
+  ethValue = firstBidding["ethValue"]
+
+  console.log(`first bidding: ${JSON.stringify(firstBidding)}`);
+
+  document.getElementById("bid-name").innerHTML = bidName;
+  document.getElementById("bidder-address").innerHTML = "by: " + bidder;
+  document.getElementById("bid-timestamp").innerHTML = "at:" + timestamp;
+  document.getElementById("bid-value-eth").innerHTML = ethValue + " ETH";
+
+}
+
+function epochToLocalDatetime(timestamp) {
+  var date = new Date(0);
+  date.setUTCSeconds(timestamp);
+  console.log(`timestamp: ${timestamp} to local: ${date}`);
+  return date;
+}
+
 
 function fixData(fetchedEvent) {
     events = fetchedEvent['auctionEvents']
-    console.log("Returning events from fixData")
-    // return events[0];
-    document.getElementById("results").innerHTML= JSON.stringify(events);
+    firstEvent = events[0]
+  
+    document.getElementById("auction-event-name").innerHTML= "auction event name: <br>" + JSON.stringify(firstEvent["name"]);
+    document.getElementById("auction-event-caller").innerHTML= "auction event caller:  <br>" + JSON.stringify(firstEvent["caller"]);
+    document.getElementById("auction-event-bidder").innerHTML= "auction event bidder:  <br>" + JSON.stringify(firstEvent["bidder"]);
+
+
+
     //console.log(`first auction event => ${JSON.stringify(events[0])}`)
 }
 
