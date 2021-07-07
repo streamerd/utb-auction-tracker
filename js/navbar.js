3856e150161cabbced4108ba9cd34048910c1cec
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-07-07 20:00:23 +0200

    update menu bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 function loadNavbar() {
     toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
-toggle_open_text = 'hey!';
-toggle_close_text = '<img src="./img/utb_world-removebg.png" width="112px" height="112px">';
+toggle_open_text = '<img src="./img/utb.png" width="110px" height="110px">';
+toggle_close_text = '<img src="./img/utb.png" width="110px" height="110px">';
 
 toggle.addEventListener('click', function() {
 	nav.classList.toggle('open');
