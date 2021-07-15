2cd5d108c6abb1f45160b52cf6ea6b22d8602296
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-07-15 02:53:24 +0200

    add horizontal timeline and a few imgs

--- js/countdown.js
+++ js/countdown.js
@@ -3,6 +3,8 @@ function counter() {
     countDownDate.toLocaleDateString("en-US", {timeZone: "America/Los_Angeles"})
     countDownDate = countDownDate.getTime();
     
+    countFirst(countDownDate);
+
     setInterval(function() {
         var now = new Date()
         now.toLocaleDateString("en-US", {timeZone: "America/Los_Angeles"})
@@ -13,10 +15,27 @@ function counter() {
         var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
         var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        //var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
         var countdown = document.getElementById("tiles"); // get tag element
-        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
-        }, 1000)
+        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span>";//<span>" + seconds + "</span>"; 
+        }, 60000)
      
+}
+
+function countFirst(countDownDate) {
+    console.log("asdasdasdasdasdadasdasd");
+    var now = new Date()
+    now.toLocaleDateString("en-US", {timeZone: "America/Los_Angeles"})
+    now = now.getTime();
+
+    var timeleft = countDownDate - now;
+        
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    //var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    var countdown = document.getElementById("tiles"); // get tag element
+    countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span>";
 }
