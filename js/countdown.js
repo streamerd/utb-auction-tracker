db16c74fc4eca489e3e2bc28143569586317337e
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-07-20 23:14:58 +0200

    bring back seconds on countdown

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,6 @@ function counter() {
     countDownDate.toLocaleDateString("en-US", {timeZone: "America/Los_Angeles"})
     countDownDate = countDownDate.getTime();
     
-    countFirst(countDownDate); // run once, fill the ETA and then repeat it every minute.
 
     setInterval(function() {
         var now = new Date()
@@ -15,26 +14,11 @@ function counter() {
         var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
         var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        //var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
         var countdown = document.getElementById("tiles"); // get tag element
-        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span>";//<span>" + seconds + "</span>"; 
-        }, 60000)
+        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span> <span>" + seconds + "</span>"; 
+        }, 1000)
      
 }
 
-function countFirst(countDownDate) {
-    var now = new Date()
-    now.toLocaleDateString("en-US", {timeZone: "America/Los_Angeles"})
-    now = now.getTime();
-
-    var timeleft = countDownDate - now;
-        
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    //var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-    
-    var countdown = document.getElementById("tiles"); // get tag element
-    countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span>";
-}
